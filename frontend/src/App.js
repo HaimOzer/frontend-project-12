69519c3d6a660fd06743dcf6e37eaf910d64a467
HaimOzer <sandromoya@icloud.com>
2024-05-26 09:01:52 +0300

    import elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/Login.jsx';
+import MainPage from './components/Main.jsx';
+import NotFoundPage from './components/NotFound.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<MainPage />} />
+				<Route path='login' element={<LoginPage />} />
+				<Route path='404' element={<NotFoundPage />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App;
+
+//  <div className='App'>
+// 		<header className='App-header'>
+// 			<img src={logo} className='App-logo' alt='logo' />
+// 			<p>
+// 				Edit <code>src/App.js</code> and save to reload.
+// 			</p>
+// 			<a
+// 				className='App-link'
+// 				href='https://reactjs.org'
+// 				target='_blank'
+// 				rel='noopener noreferrer'
+// 			>
+// 				Learn React
+// 			</a>
+// 		</header>
+//  </div>
